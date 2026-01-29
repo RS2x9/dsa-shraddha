b51d2fef70f0363c01959aeb4cd59a89342f3150
RS2x9 <162338837+RS2x9@users.noreply.github.com>
2026-01-29 22:13:59 +0530

    Refactor binary conversion and output display
    Refactor decimal to binary conversion logic and improve output formatting.

--- binary-ns-6/p1.cpp
+++ binary-ns-6/p1.cpp
@@ -1,21 +1,35 @@
 // decimal to binary
-// jaise code likha hai , write the flow of it in rough
-#include <bits/stdc++.h>
-#include<cmath>
+#include <iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 int main()
 {
-    int i=0,sum=0,n;
-    cout <<"Enter number:";
+    int n;
+    cout<<"E: ";
     cin>>n;
-    int a;
-    if (n==0) cout <<sum<<endl;
-    while (n!=0)
+    int num=n;
+    vector<int> arr;
+    while(true)
     {
-        a=n%2;
-        sum=sum+(a*(int)pow(10,i));
-        i=i+1;
-        n=n/2;
+        if(n==1)
+        {
+            arr.push_back(1);
+            break;
+        }
+        else
+        {
+            arr.push_back(n%2);
+            n=n/2;
+        }
     }
-    cout <<sum<<endl;
+    
+    // reverse
+    reverse(arr.begin(), arr.end());
+    for (int i=0 ; i< arr.size() ; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
 }
