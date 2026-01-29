7b0ba1e1cecba6ddf3794f27d67e3d1f3192348a
RS2x9 <162338837+RS2x9@users.noreply.github.com>
2026-01-29 22:16:21 +0530

    Refactor binary conversion logic in p1.cpp

--- binary-ns-6/p1.cpp
+++ binary-ns-6/p1.cpp
@@ -1,25 +1,24 @@
 // decimal to binary
 #include <iostream>
 #include<vector>
-#include<algorithm>
+#include<algorithm>.  // to use algorithms like reverse 
 using namespace std;
 int main()
 {
     int n;
     cout<<"E: ";
     cin>>n;
-    int num=n;
     vector<int> arr;
     while(true)
     {
-        if(n==1)
+        if(n==1) // after repeated divions when n becomes 1
         {
-            arr.push_back(1);
+            arr.push_back(1);  // appends 1 at the end of dynamic array
             break;
         }
         else
         {
-            arr.push_back(n%2);
+            arr.push_back(n%2);  // appends at the end of dynamic array
             n=n/2;
         }
     }
