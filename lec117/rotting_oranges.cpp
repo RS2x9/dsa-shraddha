66c869620d64a8e83c509116082501e04ce6fcde
RS2x9 <rishabhsingh20059835@gmail.com>
2026-08-10 06:51:27 +0530

    initial commit

---
+++ lec117/rotting_oranges.cpp
@@ -0,0 +1,6 @@
+/*
+    ROTTING ORANGES:
+    --> LEETCODE: 994
+    --> we have  use multiple current nodes at the same time becarse rotten orangs can bbe present 
+        at multiple places at the same time
+*/
